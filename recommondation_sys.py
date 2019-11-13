520decd4aca0953f1ce84b3a9466cc6d896d53f9
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-13 14:14:57 +0000

    Removed large dataset and created my own smaller dataset of books and ratings

--- recommondation_sys.py
+++ recommondation_sys.py
@@ -2,8 +2,21 @@ import pandas as pd
 import numpy as np
 import os
 
-books = pd.read_csv("./goodbooks-10k-master/books.csv")
-ratings_data = pd.read_csv("./goodbooks-10k-master/ratings.csv")
-print(ratings_data.head())
-print(books.head())
+def readData():
+    fields = ['book_id', '']
+
+    books = pd.read_csv("./goodbooks-10k-master/books.csv")
+    print(books.head())
+    ratings_data = pd.read_csv("./goodbooks-10k-master/ratings.csv")
+    print(ratings_data.head())
+
+    # Read and update values using .at
+    print(ratings_data.at[4, 'book_id'])
+    ratings_data.at[4,'book_id'] = 123
+    print(ratings_data.at[4, 'book_id'])
+    ratings_data.to_csv("./goodbooks-10k-master/ratings.csv", mode="w", index=False)
+    print("Hi")
+
+readData()
+
 
