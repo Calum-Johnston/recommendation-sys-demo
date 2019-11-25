12f409fe6f7088b9e6329db3d63d44525ae6da28
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-25 14:07:37 +0000

    Fixed issue with multiple ratings for same book by same user

--- recommondation_sys.py
+++ recommondation_sys.py
@@ -3,13 +3,14 @@ import numpy as np
 import os
 
 def readData():
-    fields = ['book_id', '']
-
-    books = pd.read_csv("./books-dataset/books.csv")
-    print(books.head())
+    books_data = pd.read_csv("./books-dataset/books.csv")
     ratings_data = pd.read_csv("./books-dataset/ratings.csv")
-    print(ratings_data.head())
+    return books_data, ratings_data
+    
 
-readData()
+books, ratings = readData()
+
+R_df = ratings.pivot(index="user_id", columns = "book_id", values = 'rating')
+print(R_df.head())
 
 
