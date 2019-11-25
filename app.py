887d3a0b10a866b18d1560ab58ec8e7dc47a3df5
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-25 15:54:08 +0000

    Decided to go down a simplier route for developing web applications with flask

--- app.py
+++ app.py
@@ -12,15 +12,17 @@ def output():
 if __name__ == "__main__":
     app.run()
 
+
+
 def readData():
     books_data = pd.read_csv("./books-dataset/books.csv")
     ratings_data = pd.read_csv("./books-dataset/ratings.csv")
+    
+    books, ratings = readData()
+    R_df = ratings.pivot(index="user_id", columns = "book_id", values = 'rating')
+    print(R_df.head())
+
     return books_data, ratings_data
     
-
-books, ratings = readData()
-
-R_df = ratings.pivot(index="user_id", columns = "book_id", values = 'rating')
-print(R_df.head())
 
 
