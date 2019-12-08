7cd9420cb91a16e21607948c9be758a8554a5594
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-12-08 14:09:10 +0000

    Began working on login system

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import pandas as pd
 import numpy as np
-from flask import Flask
+from flask import Flask, render_template, request, json
 import os
 
 app = Flask(__name__)
 
-@app.route("/output")
-def output():
-    return "Hello World"
+@app.route("/")
+def index():
+    return render_template('index.html')   
+
+@app.route('/signUpUser', methods=['POST'])
+def signUpUser():
+    user =  request.form['username'];
+    password = request.form['password'];
+    return json.dumps({'status':'OK','user':user,'pass':password});
 
 if __name__ == "__main__":
     app.run()
 
 
 
+# Additional Functions
 def readData():
     books_data = pd.read_csv("./books-dataset/books.csv")
     ratings_data = pd.read_csv("./books-dataset/ratings.csv")
