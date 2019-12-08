928d9253f11bc69d41ebebbf32b2c99c95e34ad3
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-12-08 15:17:41 +0000

    Got basic things setup, data can be sent successfully between client and server

--- app.py
+++ app.py
@@ -5,31 +5,38 @@ import os
 
 app = Flask(__name__)
 
+###########################################################
+# MAIN FUNCTIONS
+###########################################################
 @app.route("/")
 def index():
-    return render_template('index.html')   
+    return render_template('home.html')   
 
-@app.route('/signUpUser', methods=['POST'])
-def signUpUser():
+@app.route('/signInUser', methods=['POST'])
+def signInUser():
     user =  request.form['username'];
     password = request.form['password'];
     return json.dumps({'status':'OK','user':user,'pass':password});
 
+
+###########################################################
+# ADDITIONAL FUNCTIONS
+###########################################################
+
+
+
+###########################################################
+# Run application
+###########################################################
 if __name__ == "__main__":
+
+    # Read in the required data
+    books = pd.read_csv("./books-dataset/books.csv")
+    ratings = pd.read_csv("./books-dataset/ratings.csv")
+    users = pd.read_csv("./books-dataset/users.csv")
+
+    # Run app
     app.run()
 
-
-
-# Additional Functions
-def readData():
-    books_data = pd.read_csv("./books-dataset/books.csv")
-    ratings_data = pd.read_csv("./books-dataset/ratings.csv")
-    
-    books, ratings = readData()
-    R_df = ratings.pivot(index="user_id", columns = "book_id", values = 'rating')
-    print(R_df.head())
-
-    return books_data, ratings_data
-    
 
 
--- static/js/home.js
+++ static/js/home.js
@@ -1,10 +1,11 @@
 $(function(){
-	$('button').click(function(){
-		var user = $('#inputUsername').val();
-		var pass = $('#inputPassword').val();
+	$("#login_form").on('submit', function(e){
+		e.preventDefault()
+		var user = $('#login_username').val();
+		var pass = $('#login-password').val();
 		$.ajax({
 			url: '/signInUser',
-			data: $('form').serialize(),
+			data: $('#login_form').serialize(),
 			type: 'POST',
 			success: function(response){
 				console.log(response);
